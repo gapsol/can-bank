c10776f4b7ad3f41589a26d0a214b07004e096f1
Gabriel Pavlík <gabriel.pavlik@gmail.com>
2021-12-31 22:19:43 +0100

    Initial settings

--- api-can-bank/get_config.php
+++ api-can-bank/get_config.php
@@ -1,8 +1,11 @@
 <?php
 
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'canbank');
+$json = file_get_contents("../_settings/_env_php.json");
+$params = json_decode($json, true);
+
+define('DB_SERVER', $params['db_server']);
+define('DB_USERNAME', $params['db_username']);
+define('DB_PASSWORD', $params['db_password']);
+define('DB_NAME', $params['db_name']);
 
 function isfull($var) { return $var !== ''; }
